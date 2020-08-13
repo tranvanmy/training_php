dabb78297d05616c429c70b933bdb39511348ce2
AnhTuan <44973813+DoDucAnhTuan@users.noreply.github.com>
2020-08-14 01:34:22 +0700

    Edit file

--- AnhTuan/task1.php
+++ AnhTuan/task1.php
@@ -3,29 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="image/icon1.jpg" />
     <title>Training ngày 13/08</title>
-    <link rel="shortcut icon" type="image/png" href="icon1.jpg"/>
 </head>
-<body>    
-    <?php 
-        echo "Training PHP ngày đầu tiên <br> " 
-        ?>
-    <?php echo "Em chào anh, code hơi ngu anh thông cảm :((( <br> " ; // In ra màn hình 
-        ?> 
-    <?php
-        $HoTen = 'Do Duc Anh Tuan <br>'; //Ten
-        $Tuoi = '22 <br>'; //Tuoi
-        $NoiSinh = 'Lam Thao Phu Tho <br>'; //Noi sinh
-        $CongViec = 'Intern tai VMODEV <br>'; //Cong viec
-        ?>
-    <?php
-        echo $HoTen; //In ho ten ra man hinh
-        echo $Tuoi; 
-        echo $NoiSinh;
-        echo $CongViec;
-        ?>
-    <?php define('SDT','0354456655');
-        echo SDT;
-        ?>    
+<body>
+    <div>
+            <?php 
+                echo "Training PHP ngày đầu tiên <br> " 
+            ?>
+
+            <?php 
+                echo "Em chào anh, code hơi ngu anh thông cảm :((( <br> " ; // In ra màn hình 
+            ?> 
+
+            <?php
+                $HoTen = 'Đỗ Đức Anh Tuấn <br>'; //Ten
+                $Tuoi = '22 <br>'; //Tuoi
+                $NoiSinh = 'Lâm Thao Phú Thọ <br>'; //Noi sinh
+                $CongViec = 'Intern tại VMODEV <br>'; //Cong viec
+            ?>
+            
+            <?php
+                echo "<p>Họ tên của em là : $HoTen</p>"; //In ho ten ra man hinh
+                echo "<p>Tuổi : $Tuoi</p>";//In tuổi ra màn hình 
+                echo "<p>Nơi sinh : $NoiSinh</p>";
+                echo "<p>Công việc hiện tại : $CongViec </p> ";
+            ?>
+            <?php define('SDT','0354456655');
+                echo SDT;
+            ?>  
+    </div>
 </body>
 </html>
