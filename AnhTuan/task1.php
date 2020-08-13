bade51d8bfc888834d50761d646b19cc94296136
AnhTuan <44973813+DoDucAnhTuan@users.noreply.github.com>
2020-08-13 17:45:04 +0700

    Em nop bao cao cuoi ngay a

---
+++ AnhTuan/task1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Training ngày 13/08</title>
+    <link rel="shortcut icon" type="image/png" href="icon1.jpg"/>
+</head>
+<body>    
+    <?php 
+        echo "Training PHP ngày đầu tiên <br> " 
+        ?>
+    <?php echo "Em chào anh, code hơi ngu anh thông cảm :((( <br> " ; // In ra màn hình 
+        ?> 
+    <?php
+        $HoTen = 'Do Duc Anh Tuan <br>'; //Ten
+        $Tuoi = '22 <br>'; //Tuoi
+        $NoiSinh = 'Lam Thao Phu Tho <br>'; //Noi sinh
+        $CongViec = 'Intern tai VMODEV <br>'; //Cong viec
+        ?>
+    <?php
+        echo $HoTen; //In ho ten ra man hinh
+        echo $Tuoi; 
+        echo $NoiSinh;
+        echo $CongViec;
+        ?>
+    <?php define('SDT','0354456655');
+        echo SDT;
+        ?>    
+</body>
+</html>
