f61264ae9d9e795e6d3a43d3411cab192b9e2415
totientoan <toanto.1211@gmail.com>
2020-08-13 17:37:26 +0700

    hoc php buoi 1

---
+++ ToTienToan/b3.php
@@ -0,0 +1,12 @@
+<?php 
+    // $a = 'h3';
+    // $b = 123;
+    // $c = $a + $b;
+    // var_dump(is_int($c));
+    // var_dump($c);
+
+    $a = "q123";
+    $a = (int)$a;
+    echo is_int($a);
+    echo $a;
+?>
